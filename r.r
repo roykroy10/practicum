d5adbd06a896af8e524edb60adeefdafea5e3c1c
roykroy10 <roykroy10@gmail.com>
2018-06-25 23:20:37 +0300

    Update r.r

--- r.r
+++ r.r
@@ -1,3 +1,4 @@
+options(scipen=999)
 rm(list=ls())
 setwd("C:/Users/Royka/Documents/IDC/practicum")
 
@@ -13,6 +14,18 @@ summary(train$SalePrice)
 table(train$Neighborhood)
 
 prop.table(table(train$Neighborhood))
+
+#newTable <- unique(train$Neighborhood)
+#newTable <- by(train$SalePrice,train$Neighborhood,mean)
+#new1 <- data.frame(Date=as.Date(character()),
+#                 File=character(), 
+#                 User=character(), 
+#                 stringsAsFactors=FALSE) 
+#c1 <- c(unique(train$Neighborhood))
+#rm(new1)
+#new1 = subset(unique(train$Neighborhood),subset,select=SalePrice)
+#str(new1)
+#?subset
 
 nei <- data.frame(table(train$Neighborhood))
 colnames(nei) <- c("Neighborhood", "count")
@@ -47,7 +60,29 @@ train$avglivingprice <- train$SalePrice / train$GrLivArea
 mean_pricepersqft <- tapply(train$pricepersqft, train$Neighborhood, mean)
 mean_pricepersqft <- data.frame(mean_pricepersqft)
 nei <- cbind(nei, mean_pricepersqft)
-# ?plot
+
+sd_pricepersqft <- tapply(train$pricepersqft, train$Neighborhood, sd)
+sd_pricepersqft <- data.frame(sd_pricepersqft)
+nei <- cbind(nei, sd_pricepersqft)
+
+
+mean_avglivingprice <- tapply(train$avglivingprice, train$Neighborhood, mean)
+mean_avglivingprice <- data.frame(mean_avglivingprice)
+nei <- cbind(nei, mean_avglivingprice)
+
+sd_avglivingprice <- tapply(train$avglivingprice, train$Neighborhood, sd)
+sd_avglivingprice <- data.frame(sd_avglivingprice)
+nei <- cbind(nei, sd_avglivingprice)
+
+mean_sale_price <- tapply(train$SalePrice, train$Neighborhood, mean)
+mean_sale_price <- data.frame(mean_sale_price)
+nei <- cbind(nei, mean_sale_price)
+
+sd_sale_price <- tapply(train$SalePrice, train$Neighborhood, sd)
+sd_sale_price <- data.frame(sd_sale_price)
+nei <- cbind(nei, sd_sale_price)
+
+?plot
 #install.packages("ggplot2")
 library(ggplot2)
 
@@ -75,19 +110,19 @@ ggplot(subset(train),
   facet_wrap(~Neighborhood, scales = "free",ncol = 5)
 
 
-p <- ggplot(data = train, aes(x = SalePrice)) + geom_density()
-p + facet_wrap(~Neighborhood, scales = "free")
+# p <- ggplot(data = train, aes(x = SalePrice)) + geom_density()
+# p + facet_wrap(~Neighborhood, scales = "free")
 # p <- ggplot(data = train, aes(x = SalePrice)) + geom_histogram(binwidth = 1000)
 # p + facet_wrap(~Neighborhood, scales = "free")
 
 nei <- nei[nei$count>50,] # Remove neighborhoods with less the 50 houses
+nei_filtered <- merge(train,nei) # leaves only relevant neighborhoods
 
 ggplot(subset(train), 
        aes(SalePrice, color = Neighborhood)) +
   stat_density(geom="line") +
   facet_wrap(~Neighborhood, scales = "free",ncol = 5)
 
-nei_filtered <- merge(train,nei) # leaves only relevant neighborhoods
 
 ggplot(data=nei_filtered)+aes(x=Neighborhood,y=pricepersqft)+
   geom_point(color="darkblue",alpha=0.6,position='jitter')+
@@ -107,7 +142,7 @@ ggplot(nei_filtered[nei_filtered$Neighborhood=="Edwards", ], aes(x=factor(YrSold
 
 View(nei_filtered[nei_filtered$Neighborhood=="Edwards" & nei_filtered$pricepersqft<nei_filtered$mean_pricepersqft,])
 
-install.packages("plotly")
+#install.packages("plotly")
 library(plotly)
 
 # function for computing mean, DS, max and min values
@@ -134,6 +169,7 @@ summary(grliv_reg)
 
 test_reg <- lm(SalePrice ~ GrLivArea+TotalBsmtSF+LotArea -1, data=train)
 summary(test_reg)
+test_reg['coefficients']
 
 lotarea_saleprice_reg <- lm(pricepersqft ~ GrLivArea+YearBuilt+OverallQual, data=nei_filtered) # not good results
 summary(lotarea_saleprice_reg)
@@ -142,13 +178,66 @@ ggplot(data=nei_filtered)+aes(x=GrLivArea,y=SalePrice, color = Neighborhood  )  
   geom_point(alpha=0.6)+ 
   geom_line(mapping = aes(y=predict(grliv_saleprice_reg)),size=1)
 
-
+## Took weights from test_reg
 train$weighted_size = weighted.mean(c(train$LotArea,train$TotalBsmtSF,train$GrLivArea),c(0.2107,61.6591,75.7305))
 
-
-ggplot( nei_filtered[nei_filtered$Neighborhood=="Edwards", ], aes(x=factor(YrSold) , y=SalePrice, color=YrSold))  + 
-  scale_color_gradient(low = "black", high = "red")+ stat_summary(fun.data = min.mean.sd.max, geom = "boxplot")+ geom_jitter(size=5)
 
 ggplot( nei_filtered, aes(x=factor(MoSold) , y=SalePrice, color=Neighborhood))  + 
    stat_summary(fun.data = min.mean.sd.max, geom = "boxplot")+ geom_jitter(size=5)
 
+ggplot( nei_filtered[nei_filtered$Neighborhood=="BrkSide", ], aes(x=factor(YrSold) , y=SalePrice, color=YrSold))  + 
+  scale_color_gradient(low = "black", high = "red")+ stat_summary(fun.data = min.mean.sd.max, geom = "boxplot")+ geom_jitter(size=5)
+ggplot( nei_filtered[nei_filtered$Neighborhood=="CollgCr", ], aes(x=factor(YrSold) , y=SalePrice, color=YrSold))  + 
+  scale_color_gradient(low = "black", high = "red")+ stat_summary(fun.data = min.mean.sd.max, geom = "boxplot")+ geom_jitter(size=5)
+ggplot( nei_filtered[nei_filtered$Neighborhood=="Crawfor", ], aes(x=factor(YrSold) , y=SalePrice, color=YrSold))  + 
+  scale_color_gradient(low = "black", high = "red")+ stat_summary(fun.data = min.mean.sd.max, geom = "boxplot")+ geom_jitter(size=5)
+ggplot( nei_filtered[nei_filtered$Neighborhood=="Edwards", ], aes(x=factor(YrSold) , y=SalePrice, color=YrSold))  + 
+  scale_color_gradient(low = "black", high = "red")+ stat_summary(fun.data = min.mean.sd.max, geom = "boxplot")+ geom_jitter(size=5)
+ggplot( nei_filtered[nei_filtered$Neighborhood=="Gilbert", ], aes(x=factor(YrSold) , y=SalePrice, color=YrSold))  + 
+  scale_color_gradient(low = "black", high = "red")+ stat_summary(fun.data = min.mean.sd.max, geom = "boxplot")+ geom_jitter(size=5)
+ggplot( nei_filtered[nei_filtered$Neighborhood=="NAmes", ], aes(x=factor(YrSold) , y=SalePrice, color=YrSold))  + 
+  scale_color_gradient(low = "black", high = "red")+ stat_summary(fun.data = min.mean.sd.max, geom = "boxplot")+ geom_jitter(size=5)
+ggplot( nei_filtered[nei_filtered$Neighborhood=="OldTown", ], aes(x=factor(YrSold) , y=SalePrice, color=YrSold))  + 
+  scale_color_gradient(low = "black", high = "red")+ stat_summary(fun.data = min.mean.sd.max, geom = "boxplot")+ geom_jitter(size=5)
+ggplot( nei_filtered[nei_filtered$Neighborhood=="Sawyer", ], aes(x=factor(YrSold) , y=SalePrice, color=YrSold))  + 
+  scale_color_gradient(low = "black", high = "red")+ stat_summary(fun.data = min.mean.sd.max, geom = "boxplot")+ geom_jitter(size=5)
+ggplot( nei_filtered[nei_filtered$Neighborhood=="SawyerW", ], aes(x=factor(YrSold) , y=SalePrice, color=YrSold))  + 
+  scale_color_gradient(low = "black", high = "red")+ stat_summary(fun.data = min.mean.sd.max, geom = "boxplot")+ geom_jitter(size=5)
+ggplot( nei_filtered[nei_filtered$Neighborhood=="Somerst", ], aes(x=factor(YrSold) , y=SalePrice, color=YrSold))  + 
+  scale_color_gradient(low = "black", high = "red")+ stat_summary(fun.data = min.mean.sd.max, geom = "boxplot")+ geom_jitter(size=5)
+
+#install.packages('tableHTML')
+#library(tableHTML)
+#tableHTML(nei_filtered)
+#write_tableHTML(tableHTML(nei_filtered), file = 'myfile.html')
+
+dummytable <- subset(nei_filtered, select=c("Id", "Neighborhood", "SalePrice", "YrSold")) # create subset for dummy variable table
+dummy_year <- model.matrix(~ factor(nei_filtered$YrSold) - 1)
+dummy_nei <- model.matrix(~ factor(nei_filtered$Neighborhood) - 1)
+dat <- data.frame(nei_filtered$Id, nei_filtered$SalePrice, dummy_nei,nei_filtered$YrSold)
+
+dat$YrSold[dat$nei_filtered.YrSold=='2006'] <- 1
+dat$YrSold[dat$nei_filtered.YrSold=='2007'] <- 2
+dat$YrSold[dat$nei_filtered.YrSold=='2008'] <- 3
+dat$YrSold[dat$nei_filtered.YrSold=='2009'] <- 4
+dat$YrSold[dat$nei_filtered.YrSold=='2010'] <- 5
+dat$nei_filtered.YrSold <- NULL
+
+dat$factor.nei_filtered.Neighborhood.BrkSide <- dat$factor.nei_filtered.Neighborhood.BrkSide*dat$YrSold
+dat$factor.nei_filtered.Neighborhood.CollgCr <- dat$factor.nei_filtered.Neighborhood.CollgCr*dat$YrSold
+dat$factor.nei_filtered.Neighborhood.Crawfor <- dat$factor.nei_filtered.Neighborhood.Crawfor*dat$YrSold
+dat$factor.nei_filtered.Neighborhood.Edwards <- dat$factor.nei_filtered.Neighborhood.Edwards*dat$YrSold
+dat$factor.nei_filtered.Neighborhood.Gilbert <- dat$factor.nei_filtered.Neighborhood.Gilbert*dat$YrSold
+dat$factor.nei_filtered.Neighborhood.NAmes <- dat$factor.nei_filtered.Neighborhood.NAmes*dat$YrSold
+dat$factor.nei_filtered.Neighborhood.NridgHt <- dat$factor.nei_filtered.Neighborhood.NridgHt*dat$YrSold
+dat$factor.nei_filtered.Neighborhood.NWAmes <- dat$factor.nei_filtered.Neighborhood.NWAmes*dat$YrSold
+dat$factor.nei_filtered.Neighborhood.OldTown <- dat$factor.nei_filtered.Neighborhood.OldTown*dat$YrSold
+dat$factor.nei_filtered.Neighborhood.Sawyer <- dat$factor.nei_filtered.Neighborhood.Sawyer*dat$YrSold
+dat$factor.nei_filtered.Neighborhood.SawyerW <- dat$factor.nei_filtered.Neighborhood.SawyerW*dat$YrSold
+dat$factor.nei_filtered.Neighborhood.Somerst <- dat$factor.nei_filtered.Neighborhood.Somerst*dat$YrSold
+names(df) <- substring(names(df), 34)
+nei_filtered$sale_price_sign = (nei_filtered$SalePrice - nei_filtered$mean_sale_price) / nei_filtered$sd_sale_price
+nei_filtered$avglivingprice_sign = (nei_filtered$avglivingprice - nei_filtered$mean_avglivingprice) / nei_filtered$sd_avglivingprice
+nei_filtered$pricepersqft_sign = (nei_filtered$pricepersqft - nei_filtered$mean_pricepersqft) / nei_filtered$sd_pricepersqft
+View(nei_filtered[order(nei_filtered$pricepersqft,decreasing=F)[1:5],])
+write.csv(nei_filtered, file = "nei_filtered.csv")
